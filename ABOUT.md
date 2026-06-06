b42246832757adee3f7b28534a082b32ba744a5b
MalikHw <help.malicorporation@gmail.com>
2026-06-06 23:01:45 +0100

    yes

--- ABOUT.md
+++ ABOUT.md
@@ -4,7 +4,7 @@ just makes the Delete button on your keboard USABLE on level info screen!
 **(more scenes soon)**
 
 ### Credits:
-- [MalikHw47](https://youtube.com/@MalikHw47): code
+- [MalikHw47](https://youtube.com/@MalikHw47): code (challenged himself to make it in a 1 line code)
 - [MalikHw47](https://youtube.com/@MalikHw47): idea
 - [MalikHw47](https://youtube.com/@MalikHw47): testing
 - ~~[MalikHw47](https://youtube.com/@MalikHw47)~~ his gf: logo
--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,5 @@
 
 using namespace geode::prelude;
 
+// 1 line challenge!
 class $modify(LevelInfoLayer) {void keyDown(enumKeyCodes key, double timestamp) {if (key == enumKeyCodes::KEY_Delete) {geode::createQuickPopup("Delete Level", "Are you sure you want to <cr>delete</cr> this level?", "No", "Yes", [this](auto, bool btn2) { if (btn2) { GameLevelManager::sharedState()->deleteLevel(this->m_level);}});return;} LevelInfoLayer::keyDown(key, timestamp);}};
