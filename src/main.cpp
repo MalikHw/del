41aafc0416f9648f80fc85a79a936b9ec1691d3a
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-06-06 23:17:54 +0100

    \n supermacy

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,4 @@
 
 using namespace geode::prelude;
 
-class $modify(LevelInfoLayer) { void keyDown(enumKeyCodes key, double timestamp) { if (key == enumKeyCodes::KEY_Delete) { geode::createQuickPopup("Delete Level", "Are you sure you want to <cr>delete</c> this level?", "no", "yes", [this](auto, bool b) {if (b) {GameLevelManager::sharedState()->deleteLevel(m_level); this->onBack(nullptr);}}); return;} LevelInfoLayer::keyDown(key, timestamp);}};
+class $modify(LevelInfoLayer) { void keyDown(enumKeyCodes key, double timestamp) { if (key == enumKeyCodes::KEY_Delete) { geode::createQuickPopup("Delete Level", "Are you sure you want to <cr>delete</c> this\nlevel?", "no", "yes", [this](auto, bool b) {if (b) {GameLevelManager::sharedState()->deleteLevel(m_level); this->onBack(nullptr);}}); return;} LevelInfoLayer::keyDown(key, timestamp);}};
