a21545ceb6a3d4baac3513e9287047896562b6a4
MalikHw <help.malicorporation@gmail.com>
2026-06-06 23:12:24 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,4 @@
 
 using namespace geode::prelude;
 
-// 1 line challenge!
-class $modify(LevelInfoLayer) {void keyDown(enumKeyCodes key, double timestamp) {if (key == enumKeyCodes::KEY_Delete) {geode::createQuickPopup("Delete Level", "Are you sure you want to <cr>delete</cr> this level?", "No", "Yes", [this](auto, bool btn2) { if (btn2) { GameLevelManager::sharedState()->deleteLevel(this->m_level);}});return;} LevelInfoLayer::keyDown(key, timestamp);}};
+class $modify(LevelInfoLayer) { void keyDown(enumKeyCodes key, double timestamp) { if (key == enumKeyCodes::KEY_Delete) { geode::createQuickPopup("Delete Level", "Are you sure you want to <cr>delete</c> this level?", "no", "yes", [this](auto, bool b) {if (b) {GameLevelManager::sharedState()->deleteLevel(m_level); this->onBack(nullptr);}}); return;} LevelInfoLayer::keyDown(key, timestamp);}};
